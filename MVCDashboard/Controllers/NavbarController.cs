19eeb859710a06c60855626514bfc4809de05d04
chunlei <zhang198101@126.com>
2016-09-15 19:42:55 +0800

    O:NEW Check In Test

---
+++ MVCDashboard/Controllers/NavbarController.cs
@@ -0,0 +1,20 @@
+﻿using MVCDashboard.Domain;
+using MVCDashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDashboard.Controllers
+{
+    public class NavbarController : Controller
+    {
+        // GET: Navbar
+        public ActionResult Index()
+        {
+            var data = new Data();
+            return PartialView("_Navbar", data.navbarItems().ToList());
+        }
+    }
+}
